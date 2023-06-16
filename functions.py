46a034517a95f12560ab9b588d7c385fcdab6d6f
Kwami Sitou Kudawoo <kudawoo2002@hotmail.com>
2023-06-16 12:09:53 +0000

    Add btn functionality

--- functions.py
+++ functions.py
@@ -1,10 +1,12 @@
+file_name = "todo.txt"
 
-def get_todos(filepath):
+
+def get_todos(filepath=file_name):
     with open(filepath, 'r') as file:
         todos = file.readlines()
         return todos
 
 
-def write_todo(filepath, todo_arg):
+def write_todo(todo_arg, filepath=file_name):
     with open(filepath, "w") as file:
         file.writelines(todo_arg)
--- gui.py
+++ gui.py
@@ -2,10 +2,25 @@ import PySimpleGUI as sg
 import functions
 
 label = sg.Text("Type in a Todo")
-input_box = sg.InputText(tooltip="Enter a Todo")
+input_box = sg.InputText(tooltip="Enter a Todo", key="addBtn")
 add_button = sg.Button("Add")
 
-window = sg.Window("My To-Do App", layout=[[label], [input_box, add_button]])
-window.read()
+window = sg.Window("My To-Do App",
+                   layout=[[label], [input_box, add_button]],
+                   font=("Helvetica", 20))
+
+while True:
+    event, value = window.read()
+    print(event)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todos = value["addBtn"] + "\n"
+            todos.append(new_todos)
+            functions.write_todo(todos)
+        case sg.WIN_CLOSED:
+            break
+
 window.close()
+
 
